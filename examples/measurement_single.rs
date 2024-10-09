703da5de972ca47fc0d115d3cfdb3c9d1f168b14
mimir-d <mimir-d@users.noreply.github.com>
2024-10-09 18:43:30 +0100

    add simple measurement example
    Signed-off-by: mimir-d <mimir-d@users.noreply.github.com>

---
+++ examples/measurement_single.rs
@@ -0,0 +1,47 @@
+// (c) Meta Platforms, Inc. and affiliates.
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+use anyhow::Result;
+
+use futures::FutureExt;
+use ocptv::output as tv;
+use tv::{DutInfo, Measurement, StartedTestStep, TestResult, TestRun, TestRunOutcome, TestStatus};
+
+async fn run_measure_step(step: &StartedTestStep) -> Result<TestStatus, tv::OcptvError> {
+    step.add_measurement("temperature", 42.5.into()).await?;
+    step.add_measurement_with_details(
+        &Measurement::builder("fan_speed", 1200.into())
+            .unit("rpm")
+            .build(),
+    )
+    .await?;
+
+    Ok(TestStatus::Complete)
+}
+
+/// Simple demo with some measurements taken but not referencing DUT hardware.
+#[tokio::main]
+async fn main() -> Result<()> {
+    let dut = DutInfo::builder("dut0").build();
+    TestRun::builder("simple measurement", "1.0")
+        .build()
+        .scope(dut, |r| {
+            async move {
+                r.add_step("step0")
+                    .scope(|s| run_measure_step(s).boxed())
+                    .await?;
+
+                Ok(TestRunOutcome {
+                    status: TestStatus::Complete,
+                    result: TestResult::Pass,
+                })
+            }
+            .boxed()
+        })
+        .await?;
+
+    Ok(())
+}
